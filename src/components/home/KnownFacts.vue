739a1fa2f1d19d2c5d7d372e549b49af67ad9697
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-08-27 23:13:39 +0400

    add known facts

---
+++ src/components/home/KnownFacts.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-white p-4 rounded-2xl">
+    <div class="flex justify-between">
+      <h1 class="text-base font-medium">ცნობილი ფაქტები</h1>
+      <div class="flex space-x-4">
+        <div
+          @click="handleScrollLeft"
+          class="w-8 h-8 rounded-full bg-white flex justify-center items-center shadow-arrow cursor-pointer"
+        >
+          <IconArrowLeft />
+        </div>
+        <div
+          @click="handleScrollRight"
+          class="w-8 h-8 rounded-full bg-white flex justify-center items-center shadow-arrow cursor-pointer"
+        >
+          <IconArrowRight />
+        </div>
+      </div>
+    </div>
+    <div
+      class="flex overflow-x-scroll scrollbar-none p-4 space-x-10"
+      ref="scrollEl"
+    >
+      <KnownFactsCard v-for="slider in sliders" :key="slider" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconArrowLeft from "@/components/icons/IconArrowLeft.vue";
+import IconArrowRight from "@/components/icons/IconArrowRight.vue";
+import KnownFactsCard from "@/components/home/KnownFactsCard.vue";
+import { ref } from "vue";
+
+const scrollEl = ref(null);
+const sliders = [1, 2, 3, 4, 5];
+
+const handleScrollRight = () => {
+  scrollEl.value.scrollTo({
+    left: scrollEl.value.scrollLeft + 350,
+    behavior: "smooth",
+  });
+};
+
+const handleScrollLeft = () => {
+  scrollEl.value.scrollTo({
+    left: scrollEl.value.scrollLeft - 350,
+    behavior: "smooth",
+  });
+};
+</script>
---
+++ src/components/home/KnownFactsCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-primary-light min-w-[300px] flex flex-col p-3 rounded-2xl">
+    <img :src="KnownFactsImage" alt="image" />
+    <div class="font-medium mt-2">
+      ქვემო ქართლის რეგიონში 20მლნ ლარამდე ღირებულების
+    </div>
+  </div>
+</template>
+
+<script setup>
+import KnownFactsImage from "@/assets/images/known.png";
+</script>
