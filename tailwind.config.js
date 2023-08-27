20236baa4371b75139a358cc1ad5b01a139f82dc
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-08-27 23:11:08 +0400

    configure tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: { max: "1700px" },
+        ls: { max: "1350px" },
+        tb: { max: "1024px" },
+        mb: { max: "375px" },
+      },
+      fontFamily: {
+        bgp: "BPG Banner Caps",
+      },
+      colors: {
+        primary: "#F0F4F5",
+        "gray-light": "#F0F4F5",
+        "purple-primary": "#585EE3",
+        "primary-light": "#F5F8FF",
+      },
+      boxShadow: {
+        arrow: "0px 0px 20px 0px #0000001A",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
