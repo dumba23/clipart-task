0f23a8c05d7c2ee6bbc4bf9c9c28e634f554bbeb
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-08-27 23:14:09 +0400

    add news

---
+++ src/components/home/NewsInfo.vue
@@ -0,0 +1,80 @@
+<template>
+  <div
+    class="max-h-[585px] tb:max-h-[280px] tb:m-0 overflow-y-scroll bg-white rounded-xl scrollbar scrollbar-thumb-gray-600"
+  >
+    <h1 class="font-bold text-xl p-4">ყველა სიახლე</h1>
+    <NewsInfoCard
+      v-for="data in staticData"
+      :date="data.date"
+      :time="data.time"
+      :description="data.description"
+    />
+  </div>
+</template>
+
+<script setup>
+import NewsInfoCard from "@/components/home/NewsInfoCard.vue";
+
+const staticData = [
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    date: "02.01.2023",
+    time: "7:30",
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+];
+</script>
+
+<style scoped>
+.scrollbar::-webkit-scrollbar {
+  width: 4px;
+}
+
+.scrollbar::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/home/NewsInfoCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col font-bgp text-sm bg-white px-4 py-2">
+    <div class="flex items-center space-x-1">
+      <IconClock />
+      <span class="text-purple-primary">{{ time }}</span>
+      <IconDot />
+      <span class="text-purple-primary">{{ date }}</span>
+    </div>
+    <div class="border-b-2 border-dashed pb-2">
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconClock from "@/components/icons/IconClock.vue";
+import IconDot from "@/components/icons/IconDot.vue";
+
+defineProps({
+  date: { required: true, type: String },
+  description: { required: true, type: String },
+  time: { required: true, type: String },
+});
+</script>
