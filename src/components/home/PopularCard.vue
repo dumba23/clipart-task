84de3982f7c9a37777d1768f2a2ced340c233981
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-08-27 23:13:52 +0400

    add popular

---
+++ src/components/home/PopularCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex justify-between font-bgp text-sm bg-white mx-5 py-2 border-b-2 border-dashed"
+  >
+    <div class="">
+      <img :src="PopularImage" alt="image" class="min-w-[96px] h-24" />
+    </div>
+    <div class="pb-2 px-4">
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PopularImage from "@/assets/images/popular.png";
+
+defineProps({
+  description: { required: true, type: String },
+});
+</script>
---
+++ src/components/home/PopularInfo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="max-h-[585px] tb:max-h-[280px] overflow-y-scroll bg-white rounded-xl scrollbar scrollbar-thumb-gray-600"
+  >
+    <h1 class="font-bold text-xl p-4">პოპულარული</h1>
+    <PopularCard v-for="data in staticData" :description="data.description" />
+  </div>
+</template>
+
+<script setup>
+import PopularCard from "@/components/home/PopularCard.vue";
+
+const staticData = [
+  {
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+  {
+    description: "დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა",
+  },
+];
+</script>
+
+<style scoped>
+.scrollbar::-webkit-scrollbar {
+  width: 4px;
+}
+
+.scrollbar::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+}
+</style>
