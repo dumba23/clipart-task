eb474fad1b5843e18a1622eb6d885021926eb510
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-08-27 23:13:29 +0400

    add articles

---
+++ src/components/home/ArticlesCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="bg-white max-w-[300px] tb:min-w-[100%] tb:max-w-full flex flex-col p-3 rounded-2xl"
+  >
+    <img
+      :src="image"
+      alt="image"
+      :class="
+        expanded ? 'min-w-[100%] max-h-[200px] object-cover rounded-2xl' : ''
+      "
+    />
+    <div class="flex mt-2 space-x-2">
+      <div class="font-semibold bg-primary-light px-2 py-2 rounded-xl text-xs">
+        12.08.2023
+      </div>
+      <div class="font-semibold bg-primary-light px-2 py-2 rounded-xl text-xs">
+        პოლიტიკა
+      </div>
+    </div>
+    <div class="font-medium mt-2">{{ description }}</div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  image: { required: true, type: String },
+  description: {
+    required: false,
+    type: String,
+    default: "ქვემო ქართლის რეგიონში 20მლნ",
+  },
+  expanded: {
+    required: false,
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
---
+++ src/components/home/ArticlesInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-[100%] tb:mt-10 mb:mt-4 bg-white rounded-2xl hidden tb:block">
+    <ArticlesCard
+      :image="ArticleImg2"
+      expanded
+      description="ქვემო ქართლის რეგიონში 20მლნ ლარამდე ღირებულების"
+    />
+  </div>
+  <div
+    class="flex justify-between mt-10 mb:mt-4 mb:flex-col mb:space-y-4 mb:mb-4"
+  >
+    <div class="flex flex-col space-y-4">
+      <ArticlesCard :image="ArticleImg" />
+      <ArticlesCard :image="ArticleImg1" />
+    </div>
+    <div class="h-full bg-white rounded-2xl tb:hidden">
+      <ArticlesCard
+        :image="ArticleImg2"
+        description="ქვემო ქართლის რეგიონში 20მლნ ლარამდე ღირებულების"
+      />
+    </div>
+    <div class="flex flex-col space-y-4">
+      <ArticlesCard :image="ArticleImg3" />
+      <ArticlesCard :image="ArticleImg4" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ArticlesCard from "@/components/home/ArticlesCard.vue";
+import ArticleImg from "@/assets/images/article.png";
+import ArticleImg1 from "@/assets/images/article1.png";
+import ArticleImg2 from "@/assets/images/article2.png";
+import ArticleImg3 from "@/assets/images/article3.png";
+import ArticleImg4 from "@/assets/images/article4.png";
+</script>
